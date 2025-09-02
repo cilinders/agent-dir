528d19591e85874b0af85004d16c248e8088fb5e
cilinders <stef1996@live.nl>
2025-09-02 14:46:47 +0200

    script for ssh-git agent setup

---
+++ scripts/sshScript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+if [ $# == 0 ] || [ $1 == "-h" ]
+then
+  echo "Usage: "
+  echo "  sshScript -git|-g   : Setup for ssh-agent for use with git."
+elif [ $1 == "-git" ] || [ $1 == "-g" ]
+then
+  echo "Starting ssh-agent."
+  eval $(ssh-agent -s)
+  echo "Adding pub key."
+  ssh-add ~/.ssh/id_ed25519-02-09-2025
+  echo "Testing key."
+  ssh -T git@github.com
+fi
