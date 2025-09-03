112020cb8a68cf6018af8892cfe253e163afae8a
cilinders <stef1996@live.nl>
2025-09-03 15:03:31 +0200

    0.3 dummychatter for container

---
+++ scripts/dummyChatScript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# == 1 ]
+then
+  DATA_SOURCE=$1
+  if [ -f $DATA_SOURCE ]
+  then
+    while IFS= read -r -u3 LINE
+    do
+      read -e -p "prompt: " PROMPT
+      echo "$PROMPT"
+      echo "$LINE"
+    done 3< $DATA_SOURCE
+  else
+    echo "No dummy data given/file does not exist."
+  fi
+fi
