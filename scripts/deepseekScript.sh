2b921873403fc684c2db2172d31a69dd8e1f9215
cilinders <stef1996@live.nl>
2025-09-08 15:24:10 +0200

    curl script for (-open)deepseek

--- scripts/deepseekScript.sh
+++ scripts/deepseekScript.sh
@@ -3,25 +3,25 @@
 if [ $# == 0 ] || [ $# -gt 2 ]
 then
   printf "Usage:\n"
-  printf "  deepseekScript config.conf message: sends querry to deepseek.\n"
+  printf "  deepseekScript config.conf message: sends querry to model API.\n"
 else
-  if [ -f $1 ] 
+  if [ -f $1 ]
   then
     source $1
     if [ -f $KEY_PATH ]
     then
       KEY=$(cat $KEY_PATH)
-      curl https://api.deepseek.com/chat/completions \
+      RESPONSE=$(curl $API_URL \
         -H "Content-Type: application/json" \
         -H "Authorization: Bearer $KEY" \
         -d '{
-               "model": "deepseek-chat",
+               "model": "'"$MODEL"'",
                "messages": [
-                  {"role": "system", "content": "You are a helpful assistant."},
-                  {"role": "system", "content": "Hello!, this message is send from inside a Bash script"}
+                  {"role": "user", "content": "'"$2"'"}
                 ],
                 "stream": false
-            }'
+            }' | jq -r '.choices[0].message.content')
+      printf "$RESPONSE\n"
     else
       printf "Key file not found.\n"
     fi
