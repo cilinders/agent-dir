00260bec560039b8cb7d17fc0570357d48ac15d3
cilinders <stef1996@live.nl>
2025-09-26 11:09:52 +0200

    fix special chars

--- scripts/ollamaScript.sh
+++ scripts/ollamaScript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ $# == 0 ] || [ $# -gt 3 ] || [ $1 == "-h" ] || [ $1 == "-help" ]
+if [ $# == 0 ] || [ $# -gt 2 ] || [ $1 == "-h" ] || [ $1 == "-help" ]
 then
   printf "Usage: \n"
-  printf "  ollamaScript -prompt|-p <configFile.conf> <message>: Writes a given message to llm using the configFile.\n"
-  printf "  ollamaScript -start|-s:                                     Starts ollama service.\n"
-  printf "  ollamaScript -stop|-st:                                    Stops ollama service.\n"
+  printf "  ollamaScript -prompt|-p <configFile.conf>: Writes a given message to llm using the configFile.\n"
+  printf "  ollamaScript -start|-s:                    Starts ollama service.\n"
+  printf "  ollamaScript -stop|-st:                    Stops ollama service.\n"
 elif [ $1 == "-prompt" ] || [ $1 == "-p" ]
 then
   RUNNING=$(pgrep ollama)
@@ -13,11 +13,14 @@ then
   then
     if [ -f $2 ]
     then
-      #TODO: fix special chars
       source $2
-      RESPONSE=$(curl -sS -d '{"stream":false,"model":"'$OLLAMA_MODEL'","PROMPT":"'"$3"'"}' \
-                   -X POST http://localhost:11434/api/generate | jq -r '.response')
-      printf "$RESPONSE\n"
+      read -rp "Prompt: " PROMPT
+      PROMPT=${PROMPT//\\/\\\\}
+      PROMPT=${PROMPT//'"'/'\"'}
+      printf 'sending %s\n' "${PROMPT@Q}"
+      RESPONSE=$(curl -sS -d '{"stream":false,"model":"'$OLLAMA_MODEL'","PROMPT":"'"$PROMPT"'"}' \
+                    -X POST http://localhost:11434/api/generate | jq -r '.response')
+      printf '%s\n' "$RESPONSE"
     else
       printf "ConfigFile not found."
     fi
