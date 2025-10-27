8ecf6ada39cd3a139c012849d61461ac588034b1
cilinders <stef1996@live.nl>
2025-10-27 10:45:09 +0100

    REMOTE_EZ push

--- scripts/commitScript.sh
+++ scripts/commitScript.sh
@@ -103,7 +103,7 @@ then
       if ! [[ $f == "." ]]
       then
         FORMATTED_CODE=$(./commitScript.sh -fp $FS_CONF $f)
-        printf "%s -> %s\n" "$f" "$FORMATTED_CODE"
+        printf "%s -> %s\n" "$f" "$FORMATTED_CODE" > $TEMP_CODE_FORMATTED
       fi
     done
   else
