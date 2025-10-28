afc4baa44ce36f95cd177f624d8c74403cbe21d4
cilinders <stef1996@live.nl>
2025-10-28 11:35:14 +0100

    REMOTE_EZ push

--- scripts/ollamaScript.sh
+++ scripts/ollamaScript.sh
@@ -54,7 +54,7 @@ then
     PROMPT="$4"
     PROMPT=${PROMPT//\\/\\\\}
     PROMPT=${PROMPT//'"'/'\"'}
-    RESPONSE=$(curl -sS -d '{"stream":false,"model":"'$OLLAMA_MODEL'","temperature":"'"$TEMPERATURE"'","system":"'"$SYSTEM"'","PROMPT":"'"$PROMPT"'"}' \
+    RESPONSE=$(curl -sS -d '{"keep_alive":0,"stream":false,"model":"'$OLLAMA_MODEL'","temperature":"'"$TEMPERATURE"'","system":"'"$SYSTEM"'","PROMPT":"'"$PROMPT"'"}' \
                   -X POST http://localhost:11434/api/generate | jq -r '.response')
     printf "%s\n" "$RESPONSE"
   else
