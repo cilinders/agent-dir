3ca96d7167826bc38d08100575d5b4676f4626c3
cilinders <stef1996@live.nl>
2025-11-06 09:22:55 +0100

    REMOTE_EZ push

--- scripts/ollamaScript.sh
+++ scripts/ollamaScript.sh
@@ -81,7 +81,9 @@ then
     PROMPT=${PROMPT//'"'/'\"'}
     #MESSAGE="$DATA_INSIDE_TEST"',{"role":"user","content":"'"$PROMPT"'"}'
     MESSAGE=$(cat data/temp_test.txt) # | jq -sR .)
-    printf "%s\n" "$MESSAGE"
+    printf "%s\n\n" "$MESSAGE"
+    MESSAGE="$MESSAGE,{\"role\":\"user\",\"content\":\"$PROMPT\"}]"
+    printf "%s\n\n" "$MESSAGE"
     #RESPONSE=$(curl -sS -d '{"stream":false,"model":"'$OLLAMA_MODEL'","temperature":'"$TEMPERATURE"',"system":"'"$SYSTEM"'","MESSAGES":['"$MESSAGE"']}' -X POST http://localhost:11434/v1/chat/completions) #| jq -r '.message.content')
     RESPONSE=$(curl -sS -d '{"stream":false,"model":"'$OLLAMA_MODEL'","temperature":'"$TEMPERATURE"',"system":"'"$SYSTEM"'","MESSAGES":'"$MESSAGE"'}' -X POST http://localhost:11434/v1/chat/completions | jq -r '.choices[].message.content')
     printf "%s\n" "$RESPONSE"
