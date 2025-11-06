20ac7074d26c51ccafaaf4f4c0a3e8b08c0cbd17
cilinders <stef1996@live.nl>
2025-11-06 11:28:47 +0100

    REMOTE_EZ push

--- scripts/commitScript.sh
+++ scripts/commitScript.sh
@@ -142,12 +142,17 @@ then
 
     #HISTORY_FORMATTED_STRING goes into the -pvc
     ./ollamaScript.sh -pvc conf/ollamaConfig_ollama3-1.conf model/conf/test.conf "Respond with the issue Name, no additional commentary." > TEMP_commit.txt
+    printf "\n" >> TEMP_commit.txt
     #TODO: G_SCRIPT ISSUE_NAME with HISTORY > print to file
     ./ollamaScript.sh -pvc conf/ollamaConfig_ollama3-1.conf model/conf/test.conf "Respond with the issue Description, NO ADDITIONAL COMMENTARY." >> TEMP_commit.txt
+    printf "\n" >> TEMP_commit.txt
     #TODO: G_SCRIPT DESCRIPTION with HISTORY > print to file
     ./ollamaScript.sh -pvc conf/ollamaConfig_ollama3-1.conf model/conf/test.conf "Respond with the issue Project-Tree, no additional commentary." >> TEMP_commit.txt
+    printf "\n" >> TEMP_commit.txt
     #TODO: G_SCRIPT STRUCTURE with HISTORY > print to file
+    printf "%s\n" "$(./ollamaScript.sh -pf conf/ollamaConfig_ollama3-1.conf model/conf/structureFormat.conf TEMP_commit.txt)"
     #TODO: G_SCRIPT CODE_CHANGE per FILE from STRUCTURE with HISTORY > print all to file
+      #TODO: extract FILE from structure
     #TODO: G_SCRIPT TESTS per FILE from STRUCTURE with HISTORY > print all to file
     #TODO: G_SCRIPT COMMIT_MESSAGE with HISTORY > print to file
   else
