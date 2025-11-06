cbb9aab256def5693d6b758bf306e04b14bb9e59
cilinders <stef1996@live.nl>
2025-11-06 16:15:29 +0100

    REMOTE_EZ push

--- scripts/commitScript.sh
+++ scripts/commitScript.sh
@@ -193,13 +193,13 @@ then
     printf "" > TEMP_commit.txt
     #HISTORY_FORMATTED_STRING goes into the -pvc
     ./ollamaScript.sh -pvc conf/ollamaConfig_ollama3-1.conf model/conf/test.conf "Respond with the git issue Name from our message thread, no additional commentary." >> TEMP_commit.txt
-    printf "\n" >> TEMP_commit.txt
+    printf "----\n" >> TEMP_commit.txt
     #TODO: G_SCRIPT ISSUE_NAME with HISTORY > print to file
     ./ollamaScript.sh -pvc conf/ollamaConfig_ollama3-1.conf model/conf/test.conf "Respond with the git issue Description from out message thread, make it oneline, no additional commentary." >> TEMP_commit.txt
-    printf "\n" >> TEMP_commit.txt
+    printf "----\n" >> TEMP_commit.txt
     #TODO: G_SCRIPT DESCRIPTION with HISTORY > print to file
     ./ollamaScript.sh -pvc conf/ollamaConfig_ollama3-1.conf model/conf/test.conf "Generate a single Project-Tree which includes all filepaths from our message thread, no additional commentary." >> TEMP_commit.txt
-    printf "\n" >> TEMP_commit.txt
+    printf "----\n" >> TEMP_commit.txt
     #TODO: G_SCRIPT STRUCTURE with HISTORY > print to file
     TREE_STRING="$(./ollamaScript.sh -pf conf/ollamaConfig_ollama3-1.conf model/conf/structureFormat.conf TEMP_commit.txt)"
     printf "%s\n" "$TREE_STRING" >> TEMP_commit.txt
