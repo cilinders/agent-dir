91bb66b8d89f22b3edb7f4b69535770029e66ced
cilinders <stef1996@live.nl>
2025-11-07 11:08:53 +0100

    REMOTE_EZ push

--- scripts/commitScript.sh
+++ scripts/commitScript.sh
@@ -217,7 +217,7 @@ then
     #TODO: G_SCRIPT CODE_CHANGE per FILE from STRUCTURE with HISTORY > print all to file
       #TODO: extract FILE from structure
     IFS=$'\n' read -d '' -r -a FILE_ARRAY <<< "$FILES_STRING"
-    for FILE in "${FILE_STRING[@]}"; do
+    for FILE in "${FILE_ARRAY[@]}"; do
       #TODO: ask llm per file
       printf "%s\n" "$FILE"
     done
