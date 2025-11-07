6e19bc0591444a8986a11552e7518aadd04758bc
cilinders <stef1996@live.nl>
2025-11-07 10:21:54 +0100

    REMOTE_EZ push

--- scripts/commitScript.sh
+++ scripts/commitScript.sh
@@ -228,8 +228,7 @@ then
       #TODO: extract FILE from structure
     IFS=$'\n' read -d '' -r -a FILE_ARRAY < $FILES_STRING
     for FILE in "${FILE_ARRAY}"; do
-      #TODO: ask llm per file
-      
+      printf "TODO: ask llm per file"
     done
     #TODO: G_SCRIPT TESTS per FILE from STRUCTURE with HISTORY > print all to file
     #TODO: G_SCRIPT COMMIT_MESSAGE with HISTORY > print to file
