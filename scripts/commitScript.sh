6e67ea752ae5ba5376be47aadc11c46219ec12ee
cilinders <stef1996@live.nl>
2025-11-11 10:05:21 +0100

    REMOTE_EZ push

--- scripts/commitScript.sh
+++ scripts/commitScript.sh
@@ -174,14 +174,14 @@ then
           #HISTORY_FORMATTED_STRING+='{"role":"user","content":'"$CONTENT_USER"'},'
           #HISTORY_FORMATTED_STRING+='{"role":"assistant","content":'"$CONTENT_ASSISTANT"'}'
           #printf '{"role":"user","content":'"%s"'},' "$CONTENT_USER"
-          printf '{"role":"user","content":'"%s"'},' "$CONTENT_USER" >> data/temp_test.txt
+          printf '{"role":"user","content":"'"%s"'"},' "$CONTENT_USER" >> data/temp_test.txt
           #printf '{"role":"assistant","content":'"%s"'}' "$CONTENT_ASSISTANT"
-          printf '{"role":"assistant","content":'"%s"'}' "$CONTENT_ASSISTANT" >> data/temp_test.txt
+          printf '{"role":"assistant","content":"'"%s"'"}' "$CONTENT_ASSISTANT" >> data/temp_test.txt
         else
           #HISTORY_FORMATTED_STRING+='{"role":"user","content":'"$CONTENT_USER"'},'
           #HISTORY_FORMATTED_STRING+='{"role":"assistant","content":'"$CONTENT_ASSISTANT"'},'
-          printf '{"role":"user","content":'"$CONTENT_USER"'},' >> data/temp_test.txt
-          printf '{"role":"assistant","content":'"$CONTENT_ASSISTANT"'},' >> data/temp_test.txt
+          printf '{"role":"user","content":"'"$CONTENT_USER"'"},' >> data/temp_test.txt
+          printf '{"role":"assistant","content":"'"$CONTENT_ASSISTANT"'"},' >> data/temp_test.txt
         fi
       fi
     done
