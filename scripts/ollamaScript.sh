54bdbb80c88c2a46dea94316a61b71d5499e14b6
cilinders <stef1996@live.nl>
2025-11-11 10:31:36 +0100

    REMOTE_EZ push

--- scripts/ollamaScript.sh
+++ scripts/ollamaScript.sh
@@ -87,10 +87,10 @@ then
     #RESPONSE=$(curl -sS -d '{"stream":false,"model":"'$OLLAMA_MODEL'","temperature":'"$TEMPERATURE"',"system":"'"$SYSTEM"'","MESSAGES":['"$MESSAGE"']}' -X POST http://localhost:11434/v1/chat/completions) #| jq -r '.message.content')
     RESPONSE=$(curl -sS -d '{"keep_alive":0,"stream":false,"model":"'$OLLAMA_MODEL'","temperature":'"$TEMPERATURE"',"system":"'"$SYSTEM"'","MESSAGES":'"$MESSAGE"'}' -X POST http://localhost:11434/api/chat) # | jq -r '.message.content')
     printf "%s\n" "$RESPONSE"
-    while :
-    do
-      sleep 1
-    done
+    #while :
+    #do
+    #  sleep 1
+    #done
   else
     printf "Config or model file not found.\n"
   fi
