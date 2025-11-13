13f117d1bf8e46a00fa4a53ce9abdb1d0762c2f1
cilinders <stef1996@live.nl>
2025-11-13 10:44:07 +0100

    REMOTE_EZ push

---
+++ scripts/projectRunnerScript.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [[ $# == 0 ]] || [[ $1 == "-h" ]]
+then
+  printf "Usage: \n"
+  printf "  projectRunnerScript -c <configFile>  :  Creates the actionFile for the using llm.\n"
+  printf "  projectRunnerScript -r <configFile>  :  Runs actionFile to start the project.\n"
+elif [[ $1 == "-c" ]]
+then
+  if [[ -f $2 ]]
+  then
+    #ACTION_FILE
+    #PROJECT_DIR
+    #CONTEXT_FILE
+    source $2
+    touch "$ACTION_FILE"
+    printf "#!/bin/bash\n" > $ACTION_FILE
+    #printf "%s\n" "$(ls -R $PROJECT_DIR)"
+    declare -a PROJECT_PATHS_RAW=()
+    declare -a PROJECT_FILES=()
+    PROJECT_PATHS_RAW+=($(ls -R $PROJECT_DIR))
+    DIR_PATH=""
+    for LINE in ${PROJECT_PATHS_RAW[@]}; do
+      printf "%s\n" "$LINE"
+      if [[ "$LINE" =~ ":" ]]
+      then
+        #DIR_PATH="${LINE:0:${#LINE}-1}/"
+        DIR_PATH="$LINE"
+        DIR_PATH=${LINE//':'/'/'}
+        DIR_PATH=${DIR_PATH//'//'/'/'}
+      elif [[ "$LINE" =~ "." ]]
+      then
+        printf "%s%s\n" "$DIR_PATH" "$LINE"
+        PROJECT_FILES+=("$DIR_PATH$LINE")
+      fi
+    done
+    printf "%s\n" "${#PROJECT_FILES[@]}"
+    touch $CONTEXT_FILE
+    printf "[" > $CONTEXT_FILE
+    #TODO: cat all files into PROJECT_CONTEXT_FILE
+    for LINE in ${PROJECT_FILES[@]}; do
+      printf "%s\n" "$LINE"
+    done
+  else
+    printf "Config file not found.\n"
+  fi
+elif [[ $1 == "-r" ]]
+then
+  if [[ -f $2 ]]
+  then
+    printf "TODO: \n"
+  else
+    printf "Config file not found.\n"
+  fi
+fi
