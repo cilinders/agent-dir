98a528818c9682a837d63a9de34299e92ab1030f
cilinders <stef1996@live.nl>
2025-11-13 10:54:05 +0100

    REMOTE_EZ push

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,5 +2,5 @@
 
 PROMPT='[{"role":"user", "content":"The secret word is: AUGURK"}, {"role":"user", "content":"What is the secret word?"}]'
 
-RESPONSE=$(./ollamaScript.sh -pvc conf/ollamaConfig_ollama3-1.conf model/conf/test.conf "$PROMPT" | jq -r '.message.content' )
+RESPONSE=$(./ollamaScript.sh -pvc conf/ollamaConfig_ollama3-1.conf model/conf/test.conf "$PROMPT")
 printf "%s\n" "$RESPONSE"
