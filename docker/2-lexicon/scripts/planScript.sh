dc164a6297d3e7e3fbac1ffb77724f833cea6225
cilinders <stef1996@live.nl>
2025-11-20 11:05:15 +0100

    REMOTE_EZ push

--- docker/2-lexicon/scripts/planScript.sh
+++ docker/2-lexicon/scripts/planScript.sh
@@ -46,7 +46,7 @@ then
       for LINE in "${LINES[@]}"; do
         printf "TASK:\n %s\n" "$LINE" >> $MESSAGE_HISTORY
         RESPONSE=$($G_SCRIPT $G_TAG $G_LLM_CONF $G_MODEL_CONF "$LINE")
-        printf "SOLUTION:\n %s\n" "$RESPONSE" >> $MESSAGE_HISTORY #$TEMP_RESOLVED_FILE
+        printf "SOLUTION:\n %s\n" "$RESPONSE" >> $TEMP_RESOLVED_FILE
       done
     else
       printf "Plan file not found.\n"
