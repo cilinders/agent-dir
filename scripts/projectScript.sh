1e1225010e5d580e79c6f04ebcba688904fccd82
cilinders <stef1996@live.nl>
2025-11-25 14:11:45 +0100

    REMOTE_EZ push

--- scripts/projectScript.sh
+++ scripts/projectScript.sh
@@ -25,7 +25,7 @@ then
     #printf "%s\n" "$DESIGN_TEXT"
     PROMPT='[{"role":"user","content":"$DESIGN_PROMPT"},{"role":"user","content":"$DESIGN_TEXT"},{"role":"user","content":"Create an appropriate name for the software product, return the name WITHOUT additional commentary."}]'
     RESPONSE=""
-    if [[ ${#RESPONSE} -gt 25]]; then
+    if [[ "${#RESPONSE}" -gt 25 ]] || [[ "${#RESPONSE}" == 0 ]]; then
       RESPONSE=$($G_SCRIPT $G_SCRIPT_TAG $G_LLM_CONF $G_MODEL_CONF "$PROMPT")
       printf "%s\n" "$RESPONSE"
       RESPONSE=${RESPONSE//' '/'_'}
