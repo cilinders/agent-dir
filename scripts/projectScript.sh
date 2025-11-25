51956a5a43d3466e79bbe8633a1c9cb2e683c168
cilinders <stef1996@live.nl>
2025-11-25 13:45:08 +0100

    REMOTE_EZ push

--- scripts/projectScript.sh
+++ scripts/projectScript.sh
@@ -17,15 +17,17 @@ then
     while IFS='' read -e -r line; do
       DESIGN_PROMPT+="$line\n"
     done < "$DESIGN_PROMPT_FILE"
-    printf "%s\n" "$DESIGN_PROMPT"
+    #printf "%s\n" "$DESIGN_PROMPT"
     DESIGN_TEXT=""
     while IFS='' read -e -r line; do
       DESIGN_TEXT+="$line\n"
     done < "$DESIGN_FILE"
-    printf "%s\n" "$DESIGN_TEXT"
+    #printf "%s\n" "$DESIGN_TEXT"
     PROMPT='[{"role":"user","content":"$DESIGN_PROMPT"},{"role":"user","content":"$DESIGN_TEXT"},{"role":"user","content":"Create an appropriate name for the software product, return the name WITHOUT additional commentary."}]'
     RESPONSE=$($G_SCRIPT $G_SCRIPT_TAG $G_LLM_CONF $G_MODEL_CONF "$PROMPT")
     printf "%s\n" "$RESPONSE"
+    printf 'PROJECT_DIR="%s/src"\n' "$RESPONSE"
+    printf 'PROJECT_DIR="%s/src"\n' "$RESPONSE" >> $PROJECT_CONFIG
   else
     printf "Config file not found.\n"
   fi
