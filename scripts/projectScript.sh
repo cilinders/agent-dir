9552446949746009dbe8e177ca01bb5331b8270d
cilinders <stef1996@live.nl>
2025-11-25 13:47:27 +0100

    REMOTE_EZ push

--- scripts/projectScript.sh
+++ scripts/projectScript.sh
@@ -25,6 +25,7 @@ then
     #printf "%s\n" "$DESIGN_TEXT"
     PROMPT='[{"role":"user","content":"$DESIGN_PROMPT"},{"role":"user","content":"$DESIGN_TEXT"},{"role":"user","content":"Create an appropriate name for the software product, return the name WITHOUT additional commentary."}]'
     RESPONSE=$($G_SCRIPT $G_SCRIPT_TAG $G_LLM_CONF $G_MODEL_CONF "$PROMPT")
+    RESPONSE=${RESPONSE//' '/'_'}
     printf "%s\n" "$RESPONSE"
     printf 'PROJECT_DIR="%s/src"\n' "$RESPONSE"
     printf 'PROJECT_DIR="%s/src"\n' "$RESPONSE" >> $PROJECT_CONFIG
