20265fe04eda145844a391c0d5d24c4d62789824
cilinders <stef1996@live.nl>
2025-11-26 13:51:44 +0100

    fix removed escaping escapes

--- docker/2-lexicon/scripts/designScript.sh
+++ docker/2-lexicon/scripts/designScript.sh
@@ -48,7 +48,7 @@ then
         PROMPT+="$line\n"
       done < "$3"
       touch $PROMPT_FILE
-      printf "%s\n" "$PROMPT" > $PROMPT_FILE
+      printf "%s" "$PROMPT" > $PROMPT_FILE
       PROMPT=$(echo "$PROMPT" | tr -d '\r')
       PROMPT=${PROMPT//\\/\\\\}
       PROMPT=${PROMPT//'"'/'\"'}
