81b4edd364a1677f77f51f40d3f9a342b5b8f043
cilinders <stef1996@live.nl>
2025-11-26 13:54:04 +0100

    fix removed escaping escapes

--- docker/2-lexicon/scripts/designScript.sh
+++ docker/2-lexicon/scripts/designScript.sh
@@ -44,11 +44,11 @@ then
     then
       source $2
       PROMPT=""
-      while IFS='' read -e -r line; do
+      while IFS=$'\n' read -e -r line; do
         PROMPT+="$line\n"
       done < "$3"
       touch $PROMPT_FILE
-      printf "%s" "$PROMPT" > $PROMPT_FILE
+      printf "%s\n" "$PROMPT" > $PROMPT_FILE
       PROMPT=$(echo "$PROMPT" | tr -d '\r')
       PROMPT=${PROMPT//\\/\\\\}
       PROMPT=${PROMPT//'"'/'\"'}
