3de1225591842a191828f069f6222b4afd4317fe
cilinders <stef1996@live.nl>
2025-11-28 11:59:57 +0100

    wip projectScript

--- docker/2-lexicon/scripts/ollamaScript.sh
+++ docker/2-lexicon/scripts/ollamaScript.sh
@@ -77,8 +77,8 @@ then
     #PROMPT=${PROMPT//'"'/'\"'}
     if [[ "${PROMPT:0:2}" == "[{" ]]; then
       PROMPT=${PROMPT//\\/\\\\}
-      PROMPT=${PROMPT//'"'/'\"'}
-      #printf "%s\n" "$PROMPT"
+      #PROMPT=${PROMPT//'"'/'\"'}
+      printf "%s\n" "$PROMPT" > testPROMPT.txt
       RESPONSE=$(curl -sS -d '{"keep_alive":0,"stream":false,"model":"'$OLLAMA_MODEL'","temperature":'"$TEMPERATURE"',"system":"'"$SYSTEM"'","MESSAGES":'"$PROMPT"'}' -X POST http://localhost:11434/api/chat) # | jq -r '.message.content')
       printf "%s\n" "$RESPONSE"
     else
