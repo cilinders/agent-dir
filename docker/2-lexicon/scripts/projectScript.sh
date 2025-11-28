ecc7d00e89571143191f5c45fcdf63e1cb9dc524
cilinders <stef1996@live.nl>
2025-11-28 10:38:15 +0100

    wip projectScript

--- docker/2-lexicon/scripts/projectScript.sh
+++ docker/2-lexicon/scripts/projectScript.sh
@@ -24,8 +24,9 @@ then
     done < "$DESIGN_FILE"
     #printf "%s\n" "$DESIGN_TEXT"
     PROMPT='[{"role":"user","content":"'"$DESIGN_PROMPT"'"},{"role":"assistant","content":"'"$DESIGN_TEXT"'"},{"role":"user","content":"Create an appropriate name for the software product, return the name WITHOUT additional commentary."}]'
+    printf "%s\n" "$PROMPT"
     RESPONSE=""
-    while [[ "${#RESPONSE}" -gt 25 ]] || [[ "${#RESPONSE}" == 0 ]]; do
+    while [[ "${#RESPONSE}" -gt 25 ]] || [[ "${#RESPONSE}" == 0 ]] || [[ "$RESPONSE" == "null" ]]; do
       printf "%s\n" "${#RESPONSE}"
       RESPONSE=$($G_SCRIPT $G_SCRIPT_TAG $G_LLM_CONF $G_MODEL_CONF "$PROMPT")
       printf "%s\n" "$RESPONSE"
@@ -33,6 +34,9 @@ then
       RESPONSE=$(echo "$RESPONSE" | tr '[:upper:]' '[:lower:]')
       RESPONSE=${RESPONSE//[^-a-z_]/''}
       printf "%s\n" "$RESPONSE"
+      #if ! [[ "$RESPONSE" == "null" ]]; then
+        sleep 5s
+      #fi
     done
     printf 'PROJECT_DIR="%s/src"\n' "$RESPONSE"
     printf 'PROJECT_DIR="%s/src"\n' "$RESPONSE" >> $PROJECT_CONFIG
