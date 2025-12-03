0192306ba7884b1bda849a68bc195186b12e2982
cilinders <stef1996@live.nl>
2025-12-03 10:52:35 +0100

    tweak validate design system prompt

--- docker/2-lexicon/scripts/test.sh
+++ docker/2-lexicon/scripts/test.sh
@@ -4,7 +4,7 @@ printf "1. design\n"
 ./designScript.sh -f ../conf/designConfig.conf ../data/idee.txt
 printf "1.1 validate design\n"
 #printf "1.1 TODO: fails redo 1.\n"
-./validateScript.sh -vvd ../conf/validate_designConfig.conf ../data/design_doc_raw.txt
+./validateScript.sh -vdd ../conf/validate_designConfig.conf ../data/design_doc_raw.txt
 #printf "2. dir setup\n"
 #./projectScript.sh -id ../conf/projectScriptConfig.conf
 #printf "3. issues\n"
