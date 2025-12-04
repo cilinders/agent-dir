572cfe02377677345f3055aa18393134575e371e
cilinders <stef1996@live.nl>
2025-12-04 14:42:53 +0100

    wip parseCommentary

--- docker/2-lexicon/scripts/commentaryScript.sh
+++ docker/2-lexicon/scripts/commentaryScript.sh
@@ -4,7 +4,7 @@ if [[ $# == 0 ]] || [[ $1 == "-h" ]] || [[ $1 == "-help" ]]; then
   printf "Usage:\n"
   printf "  commentaryScript -r <parseCommentaryConfig.conf> :  Generates new issues from commentary written in resolved coding tasks using llm.\n"
 elif [[ $1 == "-r" ]]; then
-  if [[ -f $1]]; then
+  if [[ -f $1 ]]; then
     #RESOLVED_PLAN
     #G_*
     source $1
