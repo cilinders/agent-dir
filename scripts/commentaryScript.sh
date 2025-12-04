a407491ca8c40e3e8ff9294848bb6a7e0b013221
cilinders <stef1996@live.nl>
2025-12-04 14:37:01 +0100

    feat: commentaryScript on codesnippets get issuefied

---
+++ scripts/commentaryScript.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ $# == 0 ]] || [[ $1 == "-h" ]] || [[ $1 == "-help" ]]; then
+  printf "Usage:\n"
+  printf "  commentaryScript -r <parseCommentaryConfig.conf> :  Generates new issues from commentary written in resolved coding tasks using llm.\n"
+elif [[ $1 == "-r" ]]; then
+  if [[ -f $1]]; then
+    #RESOLVED_PLAN
+    #G_*
+    source $1
+    declare -a LINE_ARRAY=()
+    INDEX=0
+    IFS=''
+    while read LINE; do
+      LINE_ARRAY[$INDEX]="$LINE"
+      ((++INDEX))
+    done < $RESOLVED_PLAN
+    TASK=""
+    for LINE in "$LINE_ARRAY[@]}"; do
+      if [[ "$LINE" =~ "TASK:" ]]; then
+        if ! [[ "$TASK" == "" ]]; then
+          #TODO: ask llm need action?
+          printf "sending: %s\n" "$TASK"
+          TASK=""
+        fi
+      else
+        TASK+="$LINE"
+      fi
+    done
+  else
+    printf "Config file not found.\n"
+  fi
+fi
