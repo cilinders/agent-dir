a4486a0387bf78d2331a0c423caaede92c57db3b
cilinders <stef1996@live.nl>
2025-12-09 11:31:44 +0100

    fix issueScript -fi

--- docker/2-lexicon/scripts/issueScript.sh
+++ docker/2-lexicon/scripts/issueScript.sh
@@ -75,7 +75,7 @@ then
     for LINE in "${LINES[@]}"; do
       #printf "LINE: %s\n" "$LINE"
       ISSUE_LINES+=("$LINE")
-      if [[ "$COUNT" -eq 1 ]] && [[ "$LINE" == "" ]]; then
+      if [[ "$COUNT" -eq 1 ]] && [[ "$LINE" =~ "*1" ]]; then
         printf "YES\n"
         ISSUE_LINES=()
       fi
