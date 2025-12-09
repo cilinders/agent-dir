4f6cf9b5a9522fa6953934f577b0be53c7fc95ed
cilinders <stef1996@live.nl>
2025-12-09 09:48:33 +0100

    wip validate code commentary

--- docker/2-lexicon/scripts/issueScript.sh
+++ docker/2-lexicon/scripts/issueScript.sh
@@ -4,19 +4,41 @@ if [[ $# == 0 ]] || [[ $# -gt 4 ]] || [[ $1 == "-h" ]] || [[ $1 == "-help" ]]
 then
   printf "Usage: \n"
   printf "  issueScript -g <configFile.conf> <designFile.txt>:     Generates issues from a given designFile using llm.\n"
+  printf "  issueScript -gi <configFile.conf> <promptFile.txt>:    Generates issue from a given promptfile using llm.\n"
   printf "  issueScript -fo <configFile.conf> <openIssueFile.txt>: Isolates a issue from the issueFile for use with llm.\n"
   printf "  issueScript -fi <configFile.conf> <rawIssueFile.txt>:  Formats a raw issue file into a issueFile.\n"
 elif [[ $1 == "-g" ]]
 then
   if [[ -f $2 ]] || [[ -f $3 ]]
   then
-    #TODO: HANDLE designfile being read!!
-    #TODO: splitting into 1 line does not work
-    #TODO: removing \r doesnt work
+    #TODO: printf to file loses identation, look @ ?plannerScript? for implementation
     source $2
-    $G_SCRIPT $G_SCRIPT_TAG $G_LLM_CONF $G_MODEL_CONF $3 > $RAW_ISSUE_FILE
+    RESPONSE=$($G_SCRIPT $G_SCRIPT_TAG $G_LLM_CONF $G_MODEL_CONF $3) # > $RAW_ISSUE_FILE
+    printf "%s\n" "$RESPONSE"
+    printf "" > $RAW_ISSUE_FILE
+    IFS=$'\n'
+    for LINE in ${RESPONSE[@]}; do
+      printf "%s\n" "$LINE" >> $RAW_ISSUE_FILE
+    done
   else
     printf "ConfigFile or designFile not found.\n"
+  fi
+elif [[ $1 == "-gi" ]]; then
+  if [[ -f $2 ]] || [[ -f $3 ]]; then
+    source $2
+    RESPONSE=$($G_SCRIPT $G_SCRIPT_TAG $G_LLM_CONF $G_MODEL_CONF $3)
+    touch $TEMP_ISSUE_FILE
+    #printf "%s\n" "$RESPONSE"
+    printf "" > $TEMP_ISSUE_FILE
+    IFS=$'\n'
+    for LINE in ${RESPONSE[@]}; do
+      printf "%s\n" "$LINE"
+      printf "%s\n" "$LINE" >> $TEMP_ISSUE_FILE
+    done
+    #TODO: before remove format and cat the issue to the global issue list
+    #rm $TEMP_ISSUE_FILE
+  else
+    printf "Config or prompt file not found.\n"
   fi
 elif [[ $1 == "-fo" ]]
 then
@@ -40,47 +62,31 @@ then
   if [[ -f $2 ]] || [[ -f $3 ]]
   then
     source $2
+    IFS=$'\n' read -d '' -r -a LINES < $3
     declare -a ISSUE_LINES=()
-    INDEX=0
-    IFS=''
-    while read LINE; do
-      ISSUE_LINES[$INDEX]="$LINE"
-      ((++INDEX))
-    done < $3
-    #INDEX=0
-    #for LINE in "${ISSUE_LINES[@]}"; do
-    #  printf "%s: %s" "$INDEX" "$LINE"
-    #  if [[ "$LINE" == "" ]]; then
-    #    printf "yup"
-    #  else
-    #    printf "nope"
-    #  fi
-    #  printf "\n"
-    #  ((++INDEX))
-    #done
-    INDEX=0
-#    for LINE in "${ISSUE_LINES[@]}"; do
-#      ((++INDEX))
-#      if [[ "$LINE" == "" ]]; then
-#        break
-#      fi
-#    done
+    for LINE in "${LINES[@]}"
+    do
+      if [[ "$LINE" =~ "*" ]]
+      then
+        ISSUE_LINES+=("$LINE")
+      fi
+    done
+    declare -a ISSUES=()
     ISSUE=""
     printf "" > $ISSUE_FILE
-    COUNT=0
-    for ((i = $INDEX; i < ${#ISSUE_LINES[@]}; ++i)); do
-      ((++COUNT))
+    for ((i = 0; i < ${#ISSUE_LINES[@]}; ++i))
+    do
       ISSUE_TEMP="${ISSUE_LINES[$i]}"
       ISSUE+="${ISSUE_TEMP//$'\n'/}"
-      if [[ "${ISSUE_LINES[$i]}" == "" ]]; then
-        if [[ "$COUNT" -gt 2 ]]; then
-          ISSUE+=";"
-          printf "%s\n" "$ISSUE" >> $ISSUE_FILE
-        fi
-        COUNT=0
+      i_TEMP=$(($i+1))
+      i_TEMP=$(($i_TEMP%4))
+      if [[ $i_TEMP == 0 ]]
+      then
+        ISSUE+=";"
+        printf "%s\n" "$ISSUE" >> $ISSUE_FILE
         ISSUE=""
       else
-        ISSUE+="\n"
+        ISSUE+=","
       fi
     done
   else
